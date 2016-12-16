1b4471378f11ee3ffc957334d205bb862c8f47f3
guhongyan <1569602866@qq.com>
2016-12-16 15:58:37 +0800

    rm

--- script/life.js
+++
@@ -1,9 +0,0 @@
-function openLifeDetail(title,type){
-    api.openWin({
-        name: 'life-list',
-        url: 'life-list.html',
-        opaque: true,
-        vScrollBarEnabled: false,
-        pageParam:{title:title,type:type}
-    });
-}
